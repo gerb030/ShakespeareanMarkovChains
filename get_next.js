b4eda252be83316f04875d44bcfc5ef72b6decaf
Gerben De Graaf <gerbena@backbase.com>
2019-07-01 08:50:01 +0200

    added http server

--- get_next.js
+++ get_next.js
@@ -10,7 +10,7 @@ fs.readFile("cleaned/shakespeare.txt", "utf8", function (err, data) {
         throw err;
     }
     // generateSentence(data, 2, 4);
-    console.log("\n"+createSonnet(4, data, 2)+"\n");
+    console.log(getNextWords(data, "To", 4));
 });
 
 // function generateSentence(text, ngrams, sentences) {
@@ -21,33 +21,24 @@ fs.readFile("cleaned/shakespeare.txt", "utf8", function (err, data) {
 //     console.log(sentences);
 // }
 
-function createSonnet(lines, text, ngrams) {
-    var sonnet = "";
-    sonnet += generatePoemLine(text, ngrams, null);
-    for (var i=1;i<lines;i++) {
-        var rhymesWith = sonnet.substr(sonnet.lastIndexOf(" ")).trim().replace(/\W+/, "");
-        sonnet += "\n" + generatePoemLine(text, ngrams, rhymesWith);
-    }
-    return sonnet;
-}
-
-function generatePoemLine(text, ngrams, rhymesWith) {
-    var markov = new rita.RiMarkov(ngrams);
+function getNextWords(text, word, amount) {
+    var markov = new rita.RiMarkov(2);
     markov.loadText(text);
-    if (rhymesWith != null) {
-        var lastWordFromLine = "zyxxy";
-        var rhymingWords = [];
-        var line = "";
-        while (!rhymingWords.includes(lastWordFromLine)) {
-            line = markov.generateSentences(1)[0];
-            return line;
-            lastWordFromLine = line.substr(line.lastIndexOf(" ")).trim().replace(/\W+/, "");
-            console.log(lastWordFromLine);
-            //rhymingWords = new rita.rhymes(lastWordFromLine);                
-        }
-        return line;
+    probabilities = markov.getProbabilities('All');
+    var sorted = [];
+    for (var n in probabilities) {
+        sorted[sorted.length] = [n, probabilities[n]];
     }
-    return markov.generateSentences(1)[0];
+    for (var i=0;i<sorted.length;i++) {
+        for (var j=sorted.length-1;j>0;j--) {
+            if (sorted[j][1] > sorted[i][1]) {
+                var value = [sorted[i][0], sorted[i][1]];
+                sorted[i] = [sorted[j][0], sorted[j][1]];
+                sorted[j] = value;
+            }
+        }    
+    }
+    return sorted.splice(0, amount);
 }
 
 
